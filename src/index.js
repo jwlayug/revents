b84577f85656245eb2376a4388760c336f5971b4
James Layug <jwlayug@Jamess-iMac.local>
2018-07-30 09:55:20 +0800

    create actions, reducers for event. CRUD in redux

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./app/layout/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { configureStore } from "./app/store/configureStore";
-
+import ScrollToTop from "./app/common/util/ScrollToTop";
 const store = configureStore();
 
 const rootEl = document.getElementById("root");
@@ -15,7 +15,9 @@ let render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
       </BrowserRouter>
     </Provider>,
     rootEl
